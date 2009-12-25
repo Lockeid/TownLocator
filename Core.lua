3e4c2d4d0b3d50a544e1fcc40e16c2750bedc449
Arthur Guerard <Clayd@.(none)>
2009-12-25 18:27:23 +0100

    Got rid of Astronomer, more features

--- Core.lua
+++ Core.lua
@@ -1,11 +1,13 @@
+-- TODO: Make tooltip show up, make the OnClick handler work with map icons
+
 local TL = CreateFrame("Frame","TownLocator",UIParent)
 TL:SetScript("OnEvent", function(self,event,...) TL[event](self,...) end)
 TL:RegisterEvent"ADDON_LOADED"
+TL:RegisterEvent"WORLD_MAP_UPDATE"
 TL.L = TL_GetLocalization()
 TL.Data = TL_GetData()
 local dd = CreateFrame('Frame', 'TownLocator_Menu', UIParent, 'UIDropDownMenuTemplate')
 local Astrolabe = DongleStub"Astrolabe-0.4"
-local theWorldMap = WorldMapDetailFrame
 local miniPool = {}
 local mapPool = {}
 
@@ -35,6 +37,7 @@ TL.Types = {
 	["Mining Trainer"] = "Interface\\Minimap\\Tracking\\Profession",
 	["Skinning Trainer"] = "Interface\\Minimap\\Tracking\\Profession",
 	["Tailoring Trainer"] = "Interface\\Minimap\\Tracking\\Profession",
+	["Jewelcrafting Trainer"] = "Interface\\Minimap\\Tracking\\Profession",
 	-- Every class
 	["Mage Trainer"] = "Interface\\Minimap\\Tracking\\Class",
 	["Warrior Trainer"] = "Interface\\Minimap\\Tracking\\Class",
@@ -47,43 +50,138 @@ TL.Types = {
 	["Shaman Trainer"] = "Interface\\Minimap\\Tracking\\Class",
 	["Druid Trainer"] = "Interface\\Minimap\\Tracking\\Class",
 }
--- Hide map icons
-function TL:HideMap()
-	for _,icon in ipairs(mapPool) do
+
+-- Some utils functions
+	local conts = {"northrend","kalimdor","azeroth","expansion01", ""}
+	-- All this is stolen from Tuhljin's Astronomer
+
+    local ZoneIcons = {};
+    local continentNames = { GetMapContinents() };
+    for key, val in pairs(continentNames) do
+      local zoneNames = { GetMapZones(key) };
+      ZoneIcons[key] = {};
+      ZoneIcons[key][0] = {};
+      ZoneIcons[key][0].name = val;
+      for k, v in pairs(zoneNames) do
+        ZoneIcons[key][k] = {};
+        ZoneIcons[key][k].name = v;
+      end
+    end
+    -- Next, add entries for the "continents" whose ID numbers are 0 and -1:
+    ZoneIcons[0] = {};
+    ZoneIcons[0][0] = {}
+    ZoneIcons[0][0].name = "Azeroth";
+    ZoneIcons[-1] = {};
+    ZoneIcons[-1][0] = {}
+    ZoneIcons[-1][0].name = "Cosmic";
+ local function getzoneIDfromname(C, zoneName)
+    for k, v in ipairs(ZoneIcons[C]) do
+      if (ZoneIcons[C][k].name == zoneName) then
+        return k;
+      end
+    end
+    return nil;
+  end
+function TL:ZoneCZ(zoneName, continent)
+    local zone
+    if (type(continent) == "number") then
+      zone = getzoneIDfromname(continent, zoneName);
+      if (zone) then
+        return continent, zone;
+      else
+        return nil;
+      end
+    end
+    for k, v in pairs(ZoneIcons) do
+      zone = getzoneIDfromname(k, zoneName);
+      if (zone) then
+        return k, zone;
+      end
+    end
+    return nil;
+  end
+
+  
+-- Hide minimap icons
+local function HideMinimap()
+	for _,icon in ipairs(miniPool) do
 		Astrolabe:RemoveIconFromMinimap(icon)
 	end
 end
--- Hide minimap icons
-function TL:HideMinimap()
-	for _,frame in ipairs(miniPool) do
+-- Hide map icons
+local function HideMap()
+	for _,frame in ipairs(mapPool) do
 		frame:Hide()
 	end
 end
 -- Hide every icon
-function TL:HideAll()
-	self:HideMap()
-	self:HideMinimap()
+local function HideAll()
+	HideMap()
+	HideMinimap()
 end
 
+
 -- TomTom integration
+local function ColorGradient(perc, ...)
+	if perc >= 1 then
+		local r, g, b = select(select('#', ...) - 2, ...)
+		return r, g, b
+	elseif perc <= 0 then
+		local r, g, b = ...
+		return r, g, b
+	end
+	
+	local num = select('#', ...) / 3
+
+	local segment, relperc = math.modf(perc*(num-1))
+	local r1, g1, b1, r2, g2, b2 = select((segment*3)+1, ...)
+
+	return r1 + (r2-r1)*relperc, g1 + (g2-g1)*relperc, b1 + (b2-b1)*relperc
+end
+
+function TL:GetArrowColor(angle)
+	local perc = math.abs((math.pi - math.abs(angle)) / math.pi)
+	local r,g,b = ColorGradient(perc, 1, 0, 0, 1, 1, 0, 0, 1, 0)
+	return r, g, b
+end
+
 function TL:GoTo(icon)
 	if (not TomTom:CrazyArrowIsHijacked()) then
 		TomTom:HijackCrazyArrow(function(self, elapsed)
 			-- Angle to waypoint
-			local direction = Astrolable:GetDirectionToIcon(icon)
+			local direction = Astrolabe:GetDirectionToIcon(icon)
+			local direction = direction - GetPlayerFacing()
 			TomTom:SetCrazyArrowDirection(direction)
+			-- CrayArrowColor
+			local r,g,b = TL:GetArrowColor(direction)
+			TomTom:SetCrazyArrowColor(r,g,b)
 			-- Distance to waypoint
-			local dist, xDelta, yDelta = Astrolabe:GetDistanceToIcon(icon)
-			TomTom:SetCrazyArrowColor(0,0,0)
+			local distance, _,_ = Astrolabe:GetDistanceToIcon(icon)
 			-- CrazyArrow title
-			TomTom:SetCrazyArrowTitle("[TL]"..icon.name,dist.." yards")
+			local title = format("[TL]: %s (%d yards)", icon.name, distance)
+			TomTom:SetCrazyArrowTitle(title)
+			if distance < 5 then TomTom:ReleaseCrazyArrow() end
 		end)
 	end
 end
+
+
 -- Returns the icon
  function TL:GetTextureByType(type)
 	return TL.Types[type]
 end
+
+-- Callback
+local function OnEdge()
+	for _,icon in ipairs(miniPool) do
+		if(Astrolabe:IsIconOnEdge(icon)) then
+			icon:Hide()
+		else
+			icon:Show()
+		end
+	end
+end
+Astrolabe:Register_OnEdgeChanged_Callback(OnEdge, true)
 
 -- Scripts handlers
 local function OnEnter(self)
@@ -93,6 +191,7 @@ end
 local function OnLeave(self)
 	GameTooltip:Hide()
 end
+
 local function OnClick(self,button)
 	if(button ~= "RightButton") then
 		return
@@ -102,28 +201,29 @@ local function OnClick(self,button)
 			w = self.micon
 		else
 			w = self
-		end
+		end		
 		local tbl = {}
 		local title = {text= self.type.."\n", isTitle = true}
 		tinsert(tbl, title)
 		if(IsAddOnLoaded"TomTom") then
-			local tomtom = {text = "Go to this waypoint with TomTom", func = function() TL.GoTo(w) end}
+			local tomtom = {text = "Go to this waypoint with TomTom", func = function() TL:GoTo(self) end}
 			tinsert(tbl,tomtom)
 		end
-		local hide = {text = "Hide this POI", func = self.Hide}
+		local hide = {text = "Hide this POI", func = function() self:Hide() end}
 		tinsert(tbl,hide)
-		local hidemap = {text = "Hide all map POI", func = TL.HideMap}
+		local hidemap = {text = "Hide all map POI", func = HideMap}
 		tinsert(tbl, hidemap)
-		local hidemini = {text = "Hide all minimap POI", func = TL.HideMinimap}
-		tinsert(tbl, hideminimap)
-		local hideall = {text = "Hide all POI (map and minimap)", func = TL.HideAll}
+		local hidemini = {text = "Hide all minimap POI", func = HideMinimap}
+		tinsert(tbl, hidemini)
+		local hideall = {text = "Hide all POI (map and minimap)", func = HideAll}
 		tinsert(tbl, hideall)
 		local cancel = {text = "Cancel", func = CloseDropDownMenus}
 		tinsert(tbl, cancel)
 		
-		EasyMenu(menu,dd,self,0,0)
+		EasyMenu(tbl,dd,self,0,0)
 	end
 end
+	
 	
 -- Creates the minimap and map icon
 function TL:CreateIcons(type,zone)
@@ -134,30 +234,29 @@ function TL:CreateIcons(type,zone)
 	minimap:SetHeight((type == "Barber" and 30) or 20)
 	minimap:RegisterForClicks("RightButtonUp")
 	minimap.type = type
-	minimap.name = zone.." - "..type
 	minimap.kind = "MinimapIcon"
 	
-	minimap.icon = minimap:CreateTexture"BACKGROUND"
+	minimap.icon = minimap:CreateTexture(nil,"OVERLAY")
 	minimap.icon:SetTexture(self:GetTextureByType(type) or "Interface\\Minimap\\Tracking\\Flightmaster")
 	minimap.icon:SetPoint("CENTER",0,0)
 	minimap.icon:SetWidth((type == "Barber" and 60) or 20)
 	minimap.icon:SetHeight((type == "Barber" and 30) or 20)
-	
+		
 	minimap:SetScript("OnEnter", OnEnter)
 	minimap:SetScript("OnLeave", OnLeave)
 	minimap:SetScript("OnClick", OnClick)
+	minimap:SetScript("OnUpdate", OnUpdate)
 	
 	-- Map button
-	local map = CreateFrame("Button",nil,theWorldMap)
+	local map = CreateFrame("Button",nil,WorldMapDetailFrame)
 	map:SetWidth((type == "Barber" and 60) or 20)
 	map:SetHeight((type == "Barber" and 30) or 20)
 	map:RegisterForClicks("RightButtonUp")
 	map.type = type
-	map.name = zone.." - "..type
 	map.micon = minimap
 	map.kind = "MapIcon"
 	
-	map.icon = map:CreateTexture"BACKGROUND"
+	map.icon = map:CreateTexture(nil,"OVERLAY")
 	map.icon:SetTexture(self:GetTextureByType(type) or "Interface\\Minimap\\Tracking\\Flightmaster")
 	map.icon:SetPoint("CENTER",0,0)
 	map.icon:SetWidth((type == "Barber" and 60) or 20)
@@ -173,20 +272,28 @@ function TL:CreateIcons(type,zone)
 	
 	return minimap, map
 end
+
+
 -- Creates the POI using Astrolabe
 function TL:SetPOI(zoneName, x, y, type)	
-	continent, zone = Astronomer.ZoneCZ(zoneName)	
+	continent, zone = self:ZoneCZ(zoneName)	
 	--Flightmaster icon by default (shouldn't happen anymore)
 	local tex = self:GetTextureByType(type) or "Interface\\Minimap\\Tracking\\Flightmaster"
 	-- Because barbers rules 
 	local width = (type == "Barber" and 60) or 20
 	local height = (type == "Barber" and 30) or nil
---~ 	local icon, placed = Astronomer.NewZoneIcon(tex, width, height, continent, zone, x, y)
 
 	-- Let's try without Astronomer
-	local minimap, map = CreateIcons(type,z)
+	local minimap, map = self:CreateIcons(type,zone)
+	-- Some data 
+	minimap.name = zoneName.." - "..type
+	map.name = zoneName.." - "..type
+	coords = {continent, zone, x, y}
+	minimap.coords = coords
+	map.coords = coords
+	-- True placement
 	Astrolabe:PlaceIconOnMinimap(minimap,continent,zone,x,y)
-	Astrolabe:PlaceIconOnWorldMap(theWorldFrame,map,continent,zone,x,y)
+	Astrolabe:PlaceIconOnWorldMap(WorldMapDetailFrame,map,continent,zone,x,y)
 		
 	if(type == "Higher Learning Books") then
 		-- Little hack to see where are those damned books
@@ -194,6 +301,8 @@ function TL:SetPOI(zoneName, x, y, type)
 		map.icon:SetVertexColor(0,1,1)
 	end
 end
+
+
 -- Iterate over the data 
 function TL:IterateData()
 	for k, value in pairs(TL.Data) do
@@ -209,4 +318,23 @@ end
 function TL:ADDON_LOADED(addon) 
 	if addon ~= "TownLocator" then return end
 	TL:IterateData()
+end
+
+
+function TL:WORLD_MAP_UPDATE()
+	local mapFileName = strlower(GetMapInfo()) or ""
+	local hide = false
+	for _,cont in ipairs(conts) do
+		if mapFileName == cont then
+			hide = true
+		end
+	end
+	for _, icon in ipairs(mapPool) do 
+		Astrolabe:PlaceIconOnWorldMap(WorldMapDetailFrame, icon, unpack(icon.coords))	
+		if(hide == true) then
+			icon:SetAlpha(0)
+		else
+			icon:SetAlpha(1)
+		end
+	end
 end
